ff13347c272f17c4162818e160ef41b4f025d6df
2382034 <2382034@unai.edu>
2025-05-02 16:14:04 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ function App() {
             }
           />
           <Route
-            path="posting/:id" // Route to view recipe details
+            path="postings/:id" // Route to view recipe details
             element={
               <PrivateRoute>
                 <PostingDetail />
